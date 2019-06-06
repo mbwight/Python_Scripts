c5c8e50b5befd9bacf3472758e32a95de973f9bf
Mark Wight <31548252+mbwight@users.noreply.github.com>
2019-06-06 10:16:42 -0700

    Create averagy.py

---
+++ Average/averagy.py
@@ -0,0 +1,9 @@
+__author__ = 'Mark'
+
+number1 = input("Enter a number")
+number2 = input("Enter another number")
+number3 = input("And another")
+
+average = (int(number1) + int(number2) + int(number3)) / 3
+
+print ("The average of these numbers is " + str(average))
